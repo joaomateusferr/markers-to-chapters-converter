6b9667431737953675d4c62fc69a2e20c2965415
Joao Ferreira <joaomateusferr@gmail.com>
2023-01-08 20:55:33 -0300

    add draft

---
+++ main.py
@@ -0,0 +1,24 @@
+import os
+from tkinter import *
+from tkinter import filedialog
+
+def run():
+
+    Path = filedialog.askopenfilename(initialdir="/", title="Select A File", filetypes=(("csv files", "*.csv"),("all files", "*.*")))
+    ResultLabel.configure(text=str(Path))
+    ResultLabel.pack()
+    #FileSelectorButton.destroy()
+    os.startfile(str(Path))
+
+MainWindow = Tk()
+MainWindow.title("Convert DaVinci Resolve markers to YouTube chapters")
+MainWindow.geometry("400x400")
+
+MainFrame = Frame()
+ResultLabel = Label(MainFrame, text="")
+FileSelectorButton = Button(MainFrame, text="Click Me", command=run)
+
+MainFrame.place(relx=0.5, rely=0.5, anchor=CENTER)
+FileSelectorButton.pack()
+
+MainWindow.mainloop()
