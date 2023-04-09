533283c8b026cd64c099b09d5d308a84c5f2ee9f
Joao Ferreira <joaomateusferr@gmail.com>
2023-04-09 17:40:41 -0300

    new structure

--- main.py
+++ main.py
@@ -1,24 +1,94 @@
-import os
-from tkinter import *
-from tkinter import filedialog
+import csv
+import os.path
 
-def run():
+def parseCsvFile(CsvPath, MoreThanAnHour, ExportColor):
 
-    FilePath = filedialog.askopenfilename(initialdir="/", title="Select A File", filetypes=(("csv files", "*.csv"),("all files", "*.*")))
-    ResultLabel.configure(text=str(FilePath))
-    ResultLabel.pack()
-    #FileSelectorButton.destroy()
-    os.startfile(str(FilePath))
+    YoutubeChapters = []
 
-MainWindow = Tk()
-MainWindow.title("Convert DaVinci Resolve markers to YouTube chapters")
-MainWindow.geometry("400x400")
+    with open(CsvPath, mode='r') as CsvFile:
+        
+        CsvReader = csv.DictReader(CsvFile)
 
-MainFrame = Frame()
-ResultLabel = Label(MainFrame, text="")
-FileSelectorButton = Button(MainFrame, text="Click Me", command=run)
+        for CsvRow in CsvReader:
 
-MainFrame.place(relx=0.5, rely=0.5, anchor=CENTER)
-FileSelectorButton.pack()
+            ChapterRow = {}
 
-MainWindow.mainloop()
+            if ExportColor:
+
+                RowColor = CsvRow.get('Color')
+                
+                if RowColor is None:
+                    raise TypeError("Color column not found!")
+
+                ChapterRow['Color'] = RowColor
+
+            RowNote = CsvRow.get('Notes')
+                
+            if RowNote is None:
+                raise TypeError("Notes in column not found!")
+
+            ChapterRow['Notes'] = RowNote
+
+            RowTime = CsvRow.get('Source In')
+                
+            if RowTime is None:
+                raise TypeError("Source in column not found!")
+
+            RowTimeArray = RowTime.split(':')
+
+            if MoreThanAnHour:
+                RowTimeArray.pop(3)
+            else:
+                RowTimeArray.pop(0)
+                RowTimeArray.pop(2)
+
+            ChapterRow['Time'] = ':'.join(RowTimeArray)
+
+            YoutubeChapters.append(ChapterRow)
+
+    return YoutubeChapters
+
+def checkCsvFile(CsvPath):
+
+    Exists = os.path.exists(CsvPath)
+
+    if Exists:
+
+        IsFile = os.path.isfile(CsvPath)
+
+        if IsFile:
+
+            FileName, FileExtension = os.path.splitext(CsvPath)
+
+            if FileExtension == '.csv':
+                return True
+            else:
+                raise TypeError("File is not a csv!")
+
+        else:
+            raise TypeError("Path does not lead to a file!")
+
+    else:
+        raise TypeError("Path does not exist!")
+    
+                
+def main():
+
+    FilePath = 'timeline1.csv'
+    ExportColor = False
+    MoreThanAnHour = False
+    ExportFile = False
+    ExportFilePath = ''
+
+    try:
+
+        FileOk = checkCsvFile(FilePath)
+    
+        if FileOk :
+            YoutubeChapters = parseCsvFile(FilePath, MoreThanAnHour, ExportColor)
+            #print(str(YoutubeChapters))
+
+    except Exception as Ex: 
+        print("Something went wrong ...\n"+ str(Ex))
+
+main()
