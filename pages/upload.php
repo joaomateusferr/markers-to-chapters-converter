8181dea969a68d93325fb1fc42087068fbfa9273
Joao Ferreira <joaomateusferr@gmail.com>
2026-01-17 13:55:13 -0300

    Ui improvements

--- pages/upload.php
+++ pages/upload.php
@@ -1,38 +1,28 @@
 <form action="index.php" method="post" enctype="multipart/form-data">
-
-    <center>
-
-        <h1>Send your markers CSV file</h1>
-
-        <input type="file" name="csv" id="csv" accept=".csv,text/csv" required>
-        <label for="csv">Select file</label>
-        <div class="file-name" id="file-name">No file selected</div>
-        <div class="actions">
+    <div class="row"><h1>Send your markers CSV file</h1></div>
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <input type="file" name="csv" id="csv" accept=".csv,text/csv" required>
+        </div>
+    </div>
+    <div class="row justify-content-center">
+        <div class="col-md-4">
             <button type="submit" id="submit-btn" disabled>Process</button>
         </div>
-
-    </center>
-
+    </div>
 </form>
 
 <script>
 
     const input = document.getElementById('csv');
-    const fileName = document.getElementById('file-name');
     const submitBtn = document.getElementById('submit-btn');
 
     input.addEventListener('change', () => {
 
         if (input.files.length > 0) {
-
-            fileName.textContent = "Selected file: " + input.files[0].name;
             submitBtn.disabled = false;
-
         } else {
-
-            fileName.textContent = "No file selected";
             submitBtn.disabled = true;
-
         }
 
     });
