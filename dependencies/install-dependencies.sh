b11045521b6b61522f61902f2736cf20a3ef7b66
Joao Ferreira <joaomateusferr@gmail.com>
2026-01-17 10:13:04 -0300

    Remove old python code

--- dependencies/install-dependencies.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ $(id -u) -ne 0 ]; then #this screipt equire root privileges (root id is 0)
-   echo 'No root privileges detected!'
-   echo 'Please, run this script as root'
-else
-   apt-get update
-   apt-get -y install python3 #python3-tk
-fi
